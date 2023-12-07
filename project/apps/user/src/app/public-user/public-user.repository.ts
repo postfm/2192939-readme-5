a2440b056507a2aebb0f24295a83193c5b320494
postfm <72159645+postfm@users.noreply.github.com>
2023-12-07 19:48:55 +0300

    Имплементирует 'PublicUserRepository'

---
+++ project/apps/user/src/app/public-user/public-user.repository.ts
@@ -0,0 +1,4 @@
+import { BaseMemoryRepository } from '@project/shared/core';
+import { PublicUserEntity } from './public-user.entity';
+
+export class PublicUserRepository extends BaseMemoryRepository<PublicUserEntity> {}
