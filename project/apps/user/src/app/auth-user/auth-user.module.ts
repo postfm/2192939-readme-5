cbb9996d9dc8810435946ea78d4b9c201e2db8ff
postfm <72159645+postfm@users.noreply.github.com>
2023-12-07 19:54:41 +0300

    Добавит импорт модуля 'PublicUserModule'

--- project/apps/user/src/app/auth-user/auth-user.module.ts
+++ project/apps/user/src/app/auth-user/auth-user.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthUserController } from './auth-user.controller';
 import { AuthUserService } from './auth-user.service';
+import { PublicUserModule } from '../public-user/public-user.module';
 
 @Module({
+  imports: [PublicUserModule],
   controllers: [AuthUserController],
   providers: [AuthUserService],
 })
