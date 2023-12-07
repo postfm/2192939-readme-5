3fcf9484fb0d2ab2dd0c3cea1c9ab8ef969d26fe
postfm <72159645+postfm@users.noreply.github.com>
2023-12-07 19:28:50 +0300

    Добавит метод 'setPassword' в 'PublicUserEntity'

--- project/apps/user/src/app/public-user/public-user.entity.ts
+++ project/apps/user/src/app/public-user/public-user.entity.ts
@@ -1,5 +1,7 @@
 import { Entity } from '@project/shared/core';
 import { AuthUser } from '@project/shared/app/types';
+import { SALT_ROUNDS } from './public-user.constant';
+import { hash, genSalt } from 'bcrypt';
 
 export class PublicUserEntity implements AuthUser, Entity<string> {
   public id?: string;
@@ -25,5 +27,11 @@ export class PublicUserEntity implements AuthUser, Entity<string> {
     this.email = data.email;
     this.name = data.name;
     this.avatar = data.avatar;
+  }
+
+  public async setPassword(password: string): Promise<PublicUserEntity> {
+    const salt = await genSalt(SALT_ROUNDS);
+    this.passwordHash = await hash(password, salt);
+    return this;
   }
 }
