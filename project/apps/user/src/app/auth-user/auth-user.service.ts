4b42459be46cfd93fefca293434c54dd421bf39a
postfm <72159645+postfm@users.noreply.github.com>
2023-12-08 09:54:51 +0300

    Добавит методы 'verifyUser' и 'getUser' в 'authUserService'

--- project/apps/user/src/app/auth-user/auth-user.service.ts
+++ project/apps/user/src/app/auth-user/auth-user.service.ts
@@ -1,8 +1,18 @@
-import { Injectable, ConflictException } from '@nestjs/common';
+import {
+  Injectable,
+  ConflictException,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { PublicUserRepository } from '../public-user/public-user.repository';
 import { CreateUserDto } from './dto/create-user.dto';
-import { AUTH_USER_EXISTS } from './auth-user.constant';
+import {
+  AUTH_USER_EXISTS,
+  AUTH_USER_NOT_FOUND,
+  AUTH_USER_PASSWORD_WRONG,
+} from './auth-user.constant';
 import { PublicUserEntity } from '../public-user/public-user.entity';
+import { LoginUserDto } from './dto/login-user.dto';
 
 @Injectable()
 export class AuthUserService {
@@ -24,5 +34,25 @@ export class AuthUserService {
     );
 
     return this.publicUserRepository.save(userEntity);
+  }
+
+  public async verifyUser(dto: LoginUserDto) {
+    const { email, password } = dto;
+    const existUser = await this.publicUserRepository.findByEmail(email);
+
+    if (!existUser) {
+      throw new NotFoundException(AUTH_USER_NOT_FOUND);
+    }
+
+    const publicUserEntity = new PublicUserEntity(existUser);
+    if (!(await publicUserEntity.comparePassword)) {
+      throw new UnauthorizedException(AUTH_USER_PASSWORD_WRONG);
+    }
+
+    return publicUserEntity.toPOJO();
+  }
+
+  public async getUser(id: string) {
+    return this.publicUserRepository.findById(id);
   }
 }
