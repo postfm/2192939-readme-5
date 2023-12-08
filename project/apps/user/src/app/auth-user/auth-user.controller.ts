b8b5903c5ba67ce1c0271662684bf09603f01637
postfm <72159645+postfm@users.noreply.github.com>
2023-12-08 10:25:14 +0300

    Добавит обработчики маршрутов '/login, /id'

--- project/apps/user/src/app/auth-user/auth-user.controller.ts
+++ project/apps/user/src/app/auth-user/auth-user.controller.ts
@@ -1,8 +1,10 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { AuthUserService } from './auth-user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { fillDto } from '@project/shared/helpers';
 import { UserRdo } from './rdo/user.rdo';
+import { LoggedUserRdo } from './rdo/logged-user.rdo';
+import { LoginUserDto } from './dto/login-user.dto';
 
 @Controller('user')
 export class AuthUserController {
@@ -12,5 +14,17 @@ export class AuthUserController {
   public async create(@Body() dto: CreateUserDto) {
     const newUser = await this.authService.register(dto);
     return fillDto(UserRdo, newUser.toPOJO());
+  }
+
+  @Post('login')
+  public async login(@Body() dto: LoginUserDto) {
+    const verifiedUser = await this.authService.verifyUser(dto);
+    return fillDto(LoggedUserRdo, verifiedUser);
+  }
+
+  @Get(':id')
+  public async show(@Param('id') id: string) {
+    const existUser = await this.authService.getUser(id);
+    return fillDto(UserRdo, existUser.toPOJO());
   }
 }
