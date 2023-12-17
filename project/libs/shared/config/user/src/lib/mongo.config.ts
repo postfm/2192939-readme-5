2e88ff246921b779997292e8bd3992726b898d79
postfm <72159645+postfm@users.noreply.github.com>
2023-12-17 11:11:01 +0300

    Удалит валидацию с помощью Joi

--- project/libs/shared/config/user/src/lib/mongo.config.ts
+++ project/libs/shared/config/user/src/lib/mongo.config.ts
@@ -1,5 +1,4 @@
 import { ConfigType, registerAs } from '@nestjs/config';
-import * as Joi from 'joi';
 import { MongodbConfiguration } from './mongodb/mongodb.env';
 import { plainToClass } from 'class-transformer';
 
@@ -14,33 +13,16 @@ export interface MongoConfig {
   authBase: string;
 }
 
-const dbValidationSchema = Joi.object({
-  host: Joi.string().hostname().required(),
-  name: Joi.string().required(),
-  port: Joi.number().port().default(DEFAULT_MONGO_PORT),
-  user: Joi.string().required(),
-  password: Joi.string().required(),
-  authBase: Joi.string().required(),
-});
-
-function validateMongoConfig(config: MongoConfig): void {
-  const { error } = dbValidationSchema.validate(config, { abortEarly: true });
-
-  if (error) {
-    throw new Error(`[DB Config Validation Error]: ${error.message}`);
-  }
-}
-
 async function getDBConfig(): Promise<MongodbConfiguration> {
   const config = plainToClass(MongodbConfiguration, {
-    host: process.env.MONGO_HOST!,
-    name: process.env.MONGO_DB!,
+    host: process.env.MONGO_HOST,
+    name: process.env.MONGO_DB,
     port: process.env.MONGO_PORT
       ? parseInt(process.env.MONGO_PORT ?? `${DEFAULT_MONGO_PORT}`, 10)
       : DEFAULT_MONGO_PORT,
-    user: process.env.MONGO_USER!,
-    password: process.env.MONGO_PASSWORD!,
-    authBase: process.env.MONGO_AUTH_BASE!,
+    user: process.env.MONGO_USER,
+    password: process.env.MONGO_PASSWORD,
+    authBase: process.env.MONGO_AUTH_BASE,
   });
 
   await config.validate();
