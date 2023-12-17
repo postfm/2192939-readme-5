23e74919bc33f29aa9d6e12802ad462296fa34f8
postfm <72159645+postfm@users.noreply.github.com>
2023-12-17 07:25:50 +0300

    Обновит пространство настроек `db`

--- project/libs/shared/config/user/src/lib/mongo.config.ts
+++ project/libs/shared/config/user/src/lib/mongo.config.ts
@@ -1,5 +1,7 @@
-import { registerAs } from '@nestjs/config';
+import { ConfigType, registerAs } from '@nestjs/config';
 import * as Joi from 'joi';
+import { MongodbConfiguration } from './mongodb/mongodb.env';
+import { plainToClass } from 'class-transformer';
 
 const DEFAULT_MONGO_PORT = 27017;
 
@@ -29,18 +31,26 @@ function validateMongoConfig(config: MongoConfig): void {
   }
 }
 
-function getDBConfig(): MongoConfig {
-  const config: MongoConfig = {
+async function getDBConfig(): Promise<MongodbConfiguration> {
+  const config = plainToClass(MongodbConfiguration, {
     host: process.env.MONGO_HOST!,
     name: process.env.MONGO_DB!,
-    port: parseInt(process.env.MONGO_PORT ?? `${DEFAULT_MONGO_PORT}`, 10),
+    port: process.env.MONGO_PORT
+      ? parseInt(process.env.MONGO_PORT ?? `${DEFAULT_MONGO_PORT}`, 10)
+      : DEFAULT_MONGO_PORT,
     user: process.env.MONGO_USER!,
     password: process.env.MONGO_PASSWORD!,
     authBase: process.env.MONGO_AUTH_BASE!,
-  };
+  });
 
-  validateMongoConfig(config);
+  await config.validate();
+
   return config;
 }
 
-export default registerAs('db', getDBConfig);
+export default registerAs(
+  'db',
+  async (): Promise<ConfigType<typeof getDBConfig>> => {
+    return getDBConfig();
+  }
+);
