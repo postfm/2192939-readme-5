f89d8dc97fc365224c6cb3423c4a9143cce723c1
postfm <72159645+postfm@users.noreply.github.com>
2023-12-17 08:45:59 +0300

    Подключит `MongooseModule` в корневой модуль сервиса `account`.

--- project/apps/user/src/app/app.module.ts
+++ project/apps/user/src/app/app.module.ts
@@ -1,10 +1,19 @@
-import { ConfigUserModule } from '@project/shared/config/user';
+import {
+  ConfigUserModule,
+  getMongooseOptions,
+} from '@project/shared/config/user';
 import { Module } from '@nestjs/common';
 import { AuthUserModule } from './auth-user/auth-user.module';
 import { PublicUserModule } from './public-user/public-user.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [AuthUserModule, PublicUserModule, ConfigUserModule],
+  imports: [
+    AuthUserModule,
+    PublicUserModule,
+    ConfigUserModule,
+    MongooseModule.forRootAsync(getMongooseOptions()),
+  ],
   controllers: [],
   providers: [],
 })
