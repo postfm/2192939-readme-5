b0ad13d0789b9137ccb2a6f1cbe657655645998e
postfm <72159645+postfm@users.noreply.github.com>
2023-12-17 16:11:46 +0300

    Внедрит модель в `PublicUserModule`.

--- project/apps/user/src/app/public-user/public-user.module.ts
+++ project/apps/user/src/app/public-user/public-user.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
 import { PublicUserRepository } from './public-user.repository';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PublicUserModel, PublicUserSchema } from './public-user.model';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: PublicUserModel.name, schema: PublicUserSchema },
+    ]),
+  ],
   providers: [PublicUserRepository],
   exports: [PublicUserRepository],
 })
