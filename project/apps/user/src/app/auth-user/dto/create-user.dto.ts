0bc46c923118bf76aa475952ee4bb44388cbfb39
postfm <72159645+postfm@users.noreply.github.com>
2024-01-07 17:43:58 +0300

    Добавит базовую валидацию для DTO сервиса users

--- project/apps/user/src/app/auth-user/dto/create-user.dto.ts
+++ project/apps/user/src/app/auth-user/dto/create-user.dto.ts
@@ -1,21 +1,26 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsString } from 'class-validator';
+import { AUTH_USER_EMAIL_NOT_VALID } from '../auth-user.constant';
 
 export class CreateUserDto {
   @ApiProperty({
     description: 'User email',
     example: 'user@host.local',
   })
+  @IsEmail({}, { message: AUTH_USER_EMAIL_NOT_VALID })
   public email!: string;
 
   @ApiProperty({
     description: 'User name',
     example: 'Keks',
   })
+  @IsString()
   public name!: string;
 
   @ApiProperty({
     description: 'User password',
     example: '12345678',
   })
+  @IsString()
   public password!: string;
 }
