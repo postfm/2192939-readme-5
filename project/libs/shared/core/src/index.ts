1fbcdb70d07e2850579a3745d2e8fedc6211ad13
postfm <72159645+postfm@users.noreply.github.com>
2024-01-07 17:35:28 +0300

    Добавит базовый репозиторий для работы с `PostgreSQL`

--- project/libs/shared/core/src/index.ts
+++ project/libs/shared/core/src/index.ts
@@ -3,3 +3,4 @@ export * from './lib/repository/entity.interface';
 export * from './lib/repository/repository.interface';
 export * from './lib/repository/base-mongo-repository';
 export * from './lib/repository/addon-repository.interface';
+export * from './lib/repository/base-postgres.repository';
---
+++ project/libs/shared/core/src/lib/repository/base-postgres.repository.ts
@@ -0,0 +1,42 @@
+import { PrismaClientService } from './../../../../publication/models/src/lib/prisma-client.service';
+import { Entity, EntityIdType, DefaultPojoType } from './entity.interface';
+import { Repository } from './repository.interface';
+export abstract class BasePostgresRepository<
+  EntityType extends Entity<EntityIdType, DocumentType>,
+  DocumentType = DefaultPojoType
+> implements Repository<EntityType, DocumentType>
+{
+  constructor(
+    protected readonly client: PrismaClientService,
+    private readonly createEntity: (document: DocumentType) => EntityType
+  ) {}
+
+  protected createEntityFromDocument(
+    document: DocumentType
+  ): EntityType | null {
+    if (!document) {
+      return null;
+    }
+
+    return this.createEntity(document);
+  }
+
+  public async findById(id: EntityType['id']): Promise<EntityType | null> {
+    throw new Error('Not implemented');
+  }
+
+  public async save(entity: EntityType): Promise<EntityType> {
+    throw new Error('Not implemented');
+  }
+
+  public async update(
+    id: EntityType['id'],
+    entity: EntityType
+  ): Promise<EntityType> {
+    throw new Error('Not implemented');
+  }
+
+  public async deleteById(id: EntityType['id']): Promise<void> {
+    throw new Error('Not implemented');
+  }
+}
