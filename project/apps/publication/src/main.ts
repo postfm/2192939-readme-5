9c1f98cbc3946a353225e0199d0b1a1c2c09a40d
postfm <72159645+postfm@users.noreply.github.com>
2024-01-07 17:03:36 +0300

    Добавит документирование для сервиса публикаций

--- project/apps/publication/src/main.ts
+++ project/apps/publication/src/main.ts
@@ -7,13 +7,25 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('The "Publications" service')
+    .setDescription('Publications service API')
+    .setVersion('1.0')
+    .build();
+
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('spec', app, document);
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
+
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
   );
--- project/libs/shared/app/types/src/lib/tag/tag.interface.ts
+++
@@ -1,6 +0,0 @@
-import { Entity } from '@project/shared/core';
-
-export interface Tag extends Entity<string> {
-  title: string;
-  publicId: string;
-}
