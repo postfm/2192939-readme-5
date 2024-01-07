0551a07e0510fd924a954d39be0c67d877f313e9
postfm <72159645+postfm@users.noreply.github.com>
2024-01-07 21:03:31 +0300

    Добавит `CreatePublicDto` и `PublicRdo`

---
+++ project/apps/publication/src/app/action-public/dto/create-dto/create-public.dto.ts
@@ -0,0 +1,92 @@
+import {
+  ArrayMaxSize,
+  IsBoolean,
+  IsMongoId,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+
+export class CreatePublicDto {
+  @IsString()
+  @IsMongoId()
+  public userId: string;
+
+  // Repost
+  @IsBoolean()
+  @IsNotEmpty()
+  public isRepost: boolean;
+
+  @IsString()
+  @IsMongoId()
+  @IsNotEmpty()
+  public originalUserId: string;
+
+  // VideoPublic
+  @IsString()
+  @IsNotEmpty()
+  @IsOptional()
+  public title: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsOptional()
+  public video: string;
+
+  // TextPublic
+  @IsString()
+  @IsNotEmpty()
+  @IsOptional()
+  public header: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsOptional()
+  public notice: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsOptional()
+  public text: string;
+
+  // QuotePublic
+  @IsString()
+  @IsNotEmpty()
+  @IsOptional()
+  public quote: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsOptional()
+  public author?: string;
+
+  // PhotoPublic
+  @IsString()
+  @IsNotEmpty()
+  @IsOptional()
+  public photo: string;
+
+  // LinkPublic
+  @IsString()
+  @IsNotEmpty()
+  @IsOptional()
+  public link: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsOptional()
+  public description: string;
+
+  // Common
+  @ArrayMaxSize(8)
+  @IsOptional()
+  public tags: string[];
+
+  @IsString()
+  @IsNotEmpty()
+  public publicType: string;
+
+  @IsString()
+  @IsNotEmpty()
+  public publicStatus: string;
+}
---
+++ project/apps/publication/src/app/action-public/rdo/public.rdo.ts
@@ -0,0 +1,71 @@
+import { Expose } from 'class-transformer';
+
+export class PublicRdo {
+  @Expose()
+  public publicId: string;
+
+  @Expose()
+  public userId: string;
+
+  // Repost
+  @Expose()
+  public isRepost: string;
+
+  @Expose()
+  public originalUserId: string;
+
+  @Expose()
+  public originalPublicId: string;
+
+  // VideoPublic
+  @Expose()
+  public title: string;
+
+  @Expose()
+  public video: string;
+
+  // TextPublic
+  @Expose()
+  public header: string;
+
+  @Expose()
+  public notice: string;
+
+  @Expose()
+  public text: string;
+
+  // QuotePublic
+  @Expose()
+  public quote: string;
+
+  @Expose()
+  public author: string;
+
+  // PhotoPublic
+  @Expose()
+  public photo: string;
+
+  // LinkPublic
+  @Expose()
+  public link: string;
+
+  @Expose()
+  public description: string;
+
+  // Common
+  @Expose()
+  public tags: string[];
+
+  @Expose()
+  public comments: Comment[];
+
+  // Service
+  @Expose()
+  public createAt: string;
+
+  @Expose()
+  public publicType: string;
+
+  @Expose()
+  public publicStatus: string;
+}
