aa0aea3c4592435f1b23094c027795a2f7a6dce3
postfm <72159645+postfm@users.noreply.github.com>
2024-01-08 21:47:10 +0300

    Добавит пользовательский пайп `MongoIdValidationPipe`

--- project/libs/shared/core/src/index.ts
+++ project/libs/shared/core/src/index.ts
@@ -4,3 +4,4 @@ export * from './lib/repository/repository.interface';
 export * from './lib/repository/base-mongo-repository';
 export * from './lib/repository/addon-repository.interface';
 export * from './lib/repository/base-postgres.repository';
+export * from './lib/pipes/mongo-id-validation.pipe';
