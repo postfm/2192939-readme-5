868056f45abe6f626b9ca985867c786585ddbb09
postfm <72159645+postfm@users.noreply.github.com>
2024-01-13 10:08:29 +0300

    Добавит перечисление `RabbitRouting`.

--- project/libs/shared/app/types/src/index.ts
+++ project/libs/shared/app/types/src/index.ts
@@ -8,3 +8,4 @@ export * from './lib/token.interface';
 export * from './lib/token-payload.interface';
 export * from './lib/file.interface';
 export * from './lib/subscriber.interface';
+export * from './lib/rabbit-routing.enum';
