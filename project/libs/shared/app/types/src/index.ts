380e50d6f9fdbc9791527890a26d8702c220b47c
postfm <72159645+postfm@users.noreply.github.com>
2024-01-13 08:32:08 +0300

    Добавит интерфейс `Subscriber`.

--- project/libs/shared/app/types/src/index.ts
+++ project/libs/shared/app/types/src/index.ts
@@ -7,4 +7,4 @@ export * from './lib/public.interface';
 export * from './lib/token.interface';
 export * from './lib/token-payload.interface';
 export * from './lib/file.interface';
-export * from './lib/stored-file.interface';
+export * from './lib/subscriber.interface';
