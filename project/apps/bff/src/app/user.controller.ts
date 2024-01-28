77ba6b2780338949d81d3fd8ae672de236194060
postfm <72159645+postfm@users.noreply.github.com>
2024-01-28 07:58:54 +0300

    Добавит пример проксирования запросов `login` и `refreshToken`.

--- project/apps/bff/src/app/user.controller.ts
+++ project/apps/bff/src/app/user.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Req } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { ApplicationServiceURL } from './app.config';
+import { Request } from 'express';
+import { LoginUserDto } from './dto/login-user.dto';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly httpService: HttpService) {}
+
+  @Post('login')
+  public async login(@Body() loginUserDto: LoginUserDto) {
+    const { data } = await this.httpService.axiosRef.post(
+      `${ApplicationServiceURL.Users}/login`,
+      loginUserDto
+    );
+    return data;
+  }
+
+  @Post('refresh')
+  public async refreshToken(@Req() req: Request) {
+    const { data } = await this.httpService.axiosRef.post(
+      `${ApplicationServiceURL.Users}/refresh`,
+      null,
+      {
+        headers: {
+          Authorization: req.headers['authorization'],
+        },
+      }
+    );
+
+    return data;
+  }
+}
